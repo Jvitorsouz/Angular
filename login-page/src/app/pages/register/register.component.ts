4dd2dbcacdf3ad59034a1d548ea9974abd64172c
jviorsouz <joao.vitor.silva.souza@ccc.ufcg.edu.br>
2025-05-21 11:27:10 -0300

    Inicio

---
+++ login-page/src/app/pages/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../componets/default-login-layout/default-login-layout.component';
+
+@Component({
+  selector: 'app-register',
+  imports: [DefaultLoginLayoutComponent],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+
+}
